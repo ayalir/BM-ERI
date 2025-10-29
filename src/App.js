49341090d88866a7cb83089dd496eed11d50c73a
ayalir <ayalir13@gmail.com>
2025-10-29 22:51:51 +0100

    Modification

--- src/App.js
+++ src/App.js
@@ -11,19 +11,6 @@ export default function EriBMHome() {
   // Remplace ici avec l'URL de ton Google Form !
   const googleFormUrl = "https://forms.gle/Pc3o9o8HncpfkkaJ8";
 
-  // HOOK pour lancer la musique au premier clic
-  React.useEffect(() => {
-    const playMusic = () => {
-      const audio = document.getElementById('audio');
-      if (audio) {
-        audio.play();
-        document.removeEventListener('click', playMusic);
-      }
-    };
-    document.addEventListener('click', playMusic);
-    return () => document.removeEventListener('click', playMusic);
-  }, []);
-
   return (
     <div style={{
       position: "relative",
@@ -34,17 +21,13 @@ export default function EriBMHome() {
     }}>
       <Fonts />
 
-      {/* Audio */}
-      <audio id="audio" src="/music.mp3" preload="auto" />
-
       {/* Image Eri à gauche */}
       <img
         src="/Eri_BM.jpg"
         alt="Eri"
         style={{
           position: "absolute",
-          top: 0,
-          left: 0,
+          top: 0, left: 0,
           width: "50vw",
           height: "100vh",
           objectFit: "cover",
@@ -60,8 +43,7 @@ export default function EriBMHome() {
         alt="Sefer Thora"
         style={{
           position: "absolute",
-          top: 0,
-          right: 0,
+          top: 0, right: 0,
           width: "50vw",
           height: "100vh",
           objectFit: "cover",
@@ -82,7 +64,121 @@ export default function EriBMHome() {
         borderRadius: 18,
         boxShadow: "0 2px 28px rgba(0,0,0,0.03)"
       }}>
-        {/* ...tout le reste de ton contenu... */}
+        <div style={{ fontSize: 18, fontWeight: 600, textAlign: "center", marginBottom: 8 }}>
+          <span>Mme Ainouze Sophie</span>
+          <span style={{ margin: "0 10px" }}>&amp;</span>
+          <span>Mr Ainouze Laurent</span>
+          <div>Ayal &amp; Lirone</div>
+        </div>
+        <div style={{ fontSize: 16, textAlign: "center", marginBottom: 18 }}>
+          ont la joie de vous faire part de la Bar-Mitsva de <br /><br />
+          <span style={{
+            fontWeight: 700,
+            fontSize: 28,
+            fontFamily: "'Great Vibes', cursive",
+            color: "#2155CD",
+            letterSpacing: 2
+          }}>
+            Éri ארי
+          </span>
+        </div>
+        <div style={{ textAlign: "center", marginBottom: 14 }}>
+          Qui sera célébrée <br />
+          <span style={{
+  color: "#CFA235", // Doré élégant
+  fontWeight: 700,
+  fontSize: 19
+}}>
+  Shabbat 03 Janvier 2026 / 14 Tevet 5786 à 8h30
+</span>
+
+          <br />
+          Lecture de la Paracha Vaye’hi ויחי<br />
+          <small>L’Office sera suivi d’un apéritif dinatoire</small>
+        </div>
+        <div style={{ textAlign: "center", marginBottom: 14 }}>
+  <span
+    style={{
+      display: "block",
+      fontFamily: "'Great Vibes', cursive",
+      fontSize: 38,
+      color: "#2155CD", // ou #fff si sur fond bleu
+      fontWeight: "normal",
+      marginBottom: 4
+    }}
+  >
+    Mise des Téfilines
+  </span>
+  Dimanche 04 Janvier / 15 Tevet 5786 à 9h00<br />
+  Un petit déjeuner suivra l’Office
+</div>
+
+        <div style={{ textAlign: "center", marginBottom: 14 }}>
+  <span
+    style={{
+      display: "block",
+      fontFamily: "'Great Vibes', cursive",
+      fontSize: 34,
+      color: "#2155CD", // même couleur que Mise des Téfilines
+      fontWeight: "normal",
+      marginBottom: 4
+    }}
+  >
+    Synagogue Ozer Dalim
+  </span>
+  8 imp. Dragon<br />13006 Marseille
+</div>
+
+        <div
+  style={{
+    textAlign: "center",
+    margin: "28px 0 24px 0",
+    padding: "18px 10px",
+    background: "rgba(33,85,205,0.04)", // Optionnel : fond bleu pâle pour aérer
+    borderRadius: 12
+  }}
+>
+  <span
+    style={{
+      fontFamily: "'Quicksand', 'Arial', sans-serif",
+      fontStyle: "italic",
+      fontWeight: 500,
+      fontSize: 19,
+      color: "#2155CD",
+      marginBottom: 6,
+      display: "block"
+    }}
+  >
+    Éri vous invite à faire la fête
+  </span>
+  <span
+    style={{
+      fontFamily: "'Quicksand', 'Arial', sans-serif",
+      fontWeight: 600,
+      fontSize: 17,
+      color: "#CFA235",  // doré élégant
+      marginBottom: 4,
+      display: "block"
+    }}
+  >
+    Le Dimanche 04 Janvier 2026 à partir de 19h00
+  </span>
+  <span
+    style={{
+      fontSize: 16,
+      color: "#222",
+      fontWeight: 500,
+      display: "block"
+    }}
+  >
+    Salle Time Square
+    <br />
+    22 Rue Negresko
+    <br />
+    13008 Marseille
+  </span>
+</div>
+
         {/* RSVP bouton Google Form */}
         <div style={{ textAlign: "center", margin: "28px 0 0 0" }}>
           <h3 style={{ marginBottom: 10 }}>Réponse souhaitée dès réception</h3>
